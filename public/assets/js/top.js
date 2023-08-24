ba8b13ee8ea7963567fcae8a4c728d172ecb8a87
Kojiro Sugiyama <rogozarski.ik3@gmail.com>
2023-08-25 03:32:20 +0900

    finich all effects

--- public/assets/js/top.js
+++ public/assets/js/top.js
@@ -1,9 +1,15 @@
 $(function () {
+  Splitting();
+
+  setTimeout(function () {
+    $(".loading").fadeOut(1750);
+  }, 6000);
+
   ScrollReveal().reveal(".fadeIn", {
-    delay: 300, // アニメーション開始までの時間
-    duration: 1600, // アニメーション完了にかかる時間
+    delay: 200, // アニメーション開始までの時間
+    duration: 2000, // アニメーション完了にかかる時間
     origin: "top", // 要素がどの方向から来るか
     distance: "50px", // 移動する距離
-    reset: false, // フレームインの度に動かすか
+    reset: true, // フレームインの度に動かすか
   });
 });
