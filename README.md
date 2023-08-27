1d1bfb03f0f5f27dbd6eda18c18be145dfbf3149
Kojiro Sugiyama <rogozarski.ik3@gmail.com>
2023-08-27 17:31:34 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# LiverS GTA Server 公式サイト
+
+## 概要
+
+GTA ロールプレイサーバーである LiverS の公式 LP サイト
+
+**_URL_**
+![LiverS](https://livers-site.web.app/)
+
+## 作者
+
+[kojiro.sugayama](https://www.facebook.com/kojiro.sugayama)
+[@mui_bocchi](https://twitter.com/mui_bocchi)
--- public/assets/js/top.js
+++ public/assets/js/top.js
@@ -6,8 +6,8 @@ $(function () {
   }, 6000);
 
   ScrollReveal().reveal(".fadeIn", {
-    delay: 200, // アニメーション開始までの時間
-    duration: 2000, // アニメーション完了にかかる時間
+    delay: 300, // アニメーション開始までの時間
+    duration: 1600, // アニメーション完了にかかる時間
     origin: "top", // 要素がどの方向から来るか
     distance: "50px", // 移動する距離
     reset: true, // フレームインの度に動かすか
